2109fdb1808261fea7f41c92a777a4b6c47cfcef
Elle Ti <linh.htu@gmail.com>
2018-06-14 16:21:16 -0700

    Add stack view to labels in storyboard. Remove code in NewSecondVC -- restarting code

--- TestRealmMedium/NewSecondViewController.swift
+++ TestRealmMedium/NewSecondViewController.swift
@@ -19,16 +19,8 @@ class NewSecondViewController: UIViewController {
     @IBOutlet weak var addButton: UIButton!
     @IBOutlet weak var listTableView: UITableView!
     
-    var itemList = [CoinTokens]()
-    var tableViewDataSource = [CoinTokens]()
-    var tokenItem: CoinTokens!
-    let apiManager = APIManager()
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        tableViewDataSource = itemList
-        print("tableViewDataSource: \(tableViewDataSource)")
         
         listTableView.dataSource = self
         listTableView.delegate = self
@@ -44,7 +36,6 @@ class NewSecondViewController: UIViewController {
     func updateValues() {
         let serverAddress = URL(string: "http://localhost:3000/v1/coins_infos")
         let token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxLCJkZXZpY2VfaWQiOiJkZXZpY2UxIn0.hJUtGtgmKLdwRHEkjdhmHkrhQWW-Hnluxe6vhQmgALY"
-//        var tokenInfo = [List]()
         
         var request = URLRequest(url: serverAddress!)
         request.httpMethod = "GET"
@@ -55,6 +46,7 @@ class NewSecondViewController: UIViewController {
             let jsonData = try? JSONSerialization.jsonObject(with: data!, options: .allowFragments)
             
             print("JSON Data \(String(describing: jsonData))")
+            self.listTableView.reloadData()
         }.resume()
         
 //        let manager = APIManager()
@@ -71,9 +63,7 @@ class NewSecondViewController: UIViewController {
     }
     
     @IBAction func addButtonPressed(_ sender: UIButton) {
-        let vc = self.storyboard?.instantiateViewController(withIdentifier: "ViewController") as? ViewController
-        self.coinDelegate?.addButtonPressed(name: self.tokenItem.full_name!, symbol: self.tokenItem.symbol!, url: self.tokenItem.image_url!, time: Double(self.tokenItem.updated_at!))
-        self.navigationController?.present(vc!, animated: true, completion: nil)
+        
     }
     
     func buttonPressed(name: String, timeStamp: Double) {
@@ -88,32 +78,22 @@ class NewSecondViewController: UIViewController {
 
 extension NewSecondViewController: UITableViewDataSource, UITableViewDelegate {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return tableViewDataSource.count
+        return 1
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = listTableView.dequeueReusableCell(withIdentifier: "secondTableCell") as! NewSecondViewTableViewCell
         
-        apiManager.getTokenInfo { (info, error) in
-            DispatchQueue.main.async {
-                if error != nil {
-                    print("An error occurred here in the APIManager")
-                }
-                if let info = info {
-//                    cell.coinNameLabel.text = info.full_name
-//                    cell.coinNameSymbol.text = info.symbol?.uppercased()
-//                    cell.coinImageURL.text = info.image_url
-//                    cell.timeStampLabel.text = "\(String(describing: info.updated_at))"
-                    print("Info: \(info)")
-                }
-            }
-        }
-        
-//        let coin = tableViewDataSource[indexPath.row]
-//        cell.coinNameLabel.text = coin.name
-//        cell.coinNameSymbol.text = coin.symbol.uppercased()
-//        cell.coinImageURL.text = coin.imageURL
-//        cell.timeStampLabel.text = "\(coin.timeStamp)"
+//        apiManager.getTokenInfo { (info, error) in
+//            DispatchQueue.main.async {
+//                if error != nil {
+//                    print("An error occurred here in the APIManager")
+//                }
+//                if let info = info {
+//                    print("Info: \(info)")
+//                }
+//            }
+//        }
         
         return cell
     }
@@ -125,22 +105,5 @@ extension NewSecondViewController: UITableViewDataSource, UITableViewDelegate {
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         
-        if segue.identifier == "BackToMain" {
-            let vc = segue.destination as! ViewController
-            let selectedIP = self.listTableView.indexPathForSelectedRow!
-            let row = selectedIP.row
-            vc.itemList = [itemList[row]]
-            print("Info to pass: \(vc.itemList)")
-        }
-        
-//        let aCoin = Coin()
-//        aCoin.name = textField.text!
-//        //        aSwitch.isEnabled = aSwitch.isOn
-//
-//        try! realm.write {
-//            realm.add(aCoin)
-//        }
-//
-//        dismiss(animated: true, completion: nil)
     }
 }
